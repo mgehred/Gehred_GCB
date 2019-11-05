f0718c16bdff1e03f4002f6022a03e124e6f65d0
mgehred <mzg@duke.edu>
2019-11-05 09:49:56 -0500

    Initial version of fasta reader, MZG edited to make generalizable to all fasta files in current directory

---
+++ python-fasta_input.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def read_fasta(x):
+    """
+    Read in a file in FASTA format    
+    """
+    x = input("Which .fasta file? ")
+    seq = ''
+    f = open(x)
+    for line in f:
+        line = line.strip()
+        seq = seq + line
+    f.close()
+    return print(seq)
+read_fasta('x')
+
